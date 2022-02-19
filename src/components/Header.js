e76eaed73e634539812ce6aecc6b43c8a713c92b
Adarsh <raj21adarsh@gmail.com>
2022-02-19 19:30:40 +0530

    deploying app to gh pages

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header>
+      <video src="/video.mp4" loop autoPlay muted></video>
+      <h1>Description</h1>
+      <div className="headerbg"></div>
+    </header>
+  );
+}
+export default Header;
